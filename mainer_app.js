5c8fc49ad7436a172c1d9e1078196b90820320c0
Benjamin Bardy <benji_ba@hotmail.fr>
2019-04-14 12:16:04 +0200

    Ajout fichiers dépendance

---
+++ mainer_app.js
@@ -0,0 +1,52 @@
+var path = require('path');
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var mongoose = require('mongoose');
+
+var database = require("./databases/hebergements_data.json");
+
+const DB_NAME = "test";
+const url = "mongodb://localhost/"+DB_NAME;
+
+app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+mongoose.connect(url, {useNewUrlParser : true}, (err) => {
+	if (err) throw err;
+
+	console.log("Database connected !");
+});
+
+var testSchema = mongoose.Schema({
+	param1 : String,
+	param2 : String,
+	param3 : String
+});
+
+
+var Test = mongoose.model("Test", testSchema, "data");
+
+
+app.get("/", (request, response) => {
+	response.sendFile(path.resolve(__dirname, "pages/main_page.html"));
+
+})
+
+
+app.post("/", (req,res) => {
+	new Test(req.body).save((err) => {
+		if (err) throw err;
+		console.log("test added !");
+		Test.find().then(profiles => console.log(profiles.length))
+	})
+
+	//console.log(req.body);
+	res.redirect("/");
+
+})
+
+app.listen(8000, () => {console.log("Connected to localhost on port 8000");});
