766e9eeffaf6d1fa7d33ee7e99484d05adbaf70f
Arceus-San <maxime8198@gmail.com>
2019-05-04 17:02:55 +0200

    debut graphes

--- pages/scripthtml.js
+++ pages/scripthtml.js
@@ -339,7 +339,86 @@ $(document).ready(function() {
 				$("#pd").append("<img src='Icons_markers/tel.png' style='padding:5px;'>"+tel+"<br>");
 				
 				
-				
+				function getRandomInt(max) {
+  				return Math.floor(Math.random() * Math.floor(max));
+				}
+
+				var sport = getRandomInt(50);
+				var patrimoine = getRandomInt(50);
+				var degustation = getRandomInt(50);
+
+				var margin = {top: 40, right: 20, bottom: 30, left: 40},
+				    width = 200 - margin.left - margin.right,
+				    height = 100 - margin.top - margin.bottom;
+
+				var formatPercent = d3.format(".0%");
+
+				var x = d3.scale.ordinal()
+				    .rangeRoundBands([0, width], .1);
+
+				var y = d3.scale.linear()
+				    .range([height, 0]);
+
+				var xAxis = d3.svg.axis()
+				    .scale(x)
+				    .orient("bottom");
+
+				var yAxis = d3.svg.axis()
+				    .scale(y)
+				    .orient("left")
+				    .tickFormat(formatPercent);
+
+				var tip = d3.tip()
+				  .attr('class', 'd3-tip')
+				  .offset([-10, 0])
+				  .html(function(d) {
+				    return "<strong>Frequency:</strong> <span style='color:red'>" + d.frequency + "</span>";
+				  })
+
+				var svg = d3.select("#pd").append("svg")
+				    .attr("width", width + margin.left + margin.right)
+				    .attr("height", height + margin.top + margin.bottom)
+				  .append("g")
+				    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+				svg.call(tip);
+
+				d3.tsv("data.tsv", type, function(error, data) {
+				  x.domain(data.map(function(d) { return d.letter; }));
+				  y.domain([0, d3.max(data, function(d) { return d.frequency; })]);
+
+				  svg.append("g")
+				      .attr("class", "x axis")
+				      .attr("transform", "translate(0," + height + ")")
+				      .call(xAxis);
+
+				  svg.append("g")
+				      .attr("class", "y axis")
+				      .call(yAxis)
+				    .append("text")
+				      .attr("transform", "rotate(-90)")
+				      .attr("y", 6)
+				      .attr("dy", ".71em")
+				      .style("text-anchor", "end")
+				      .text("Frequency");
+
+				  svg.selectAll(".bar")
+				      .data(data)
+				    .enter().append("rect")
+				      .attr("class", "bar")
+				      .attr("x", function(d) { return x(d.letter); })
+				      .attr("width", x.rangeBand())
+				      .attr("y", function(d) { return y(d.frequency); })
+				      .attr("height", function(d) { return height - y(d.frequency); })
+				      .on('mouseover', tip.show)
+				      .on('mouseout', tip.hide)
+
+				});
+
+				function type(d) {
+				  d.frequency = +d.frequency;
+				  return d;
+				}
 				
 			};
 			
@@ -364,7 +443,6 @@ $(document).ready(function() {
 				var type = L.marker(coor2,{icon: Icon}).bindPopup(popup).addTo(cities);
 				}
 				console.log(cities);
-<!-- 				
 			}
 			layer(r);  
 			
